20448f202ee6d62c2cc4fe1e77d708b033ad10b3
kleyson castro <kleysoncastro99@gmail.com>
2020-07-13 15:41:35 -0400

    change in the "method adiciona" algorithm

--- src/com/castro/estrutura/vetor/Vetor.java
+++ src/com/castro/estrutura/vetor/Vetor.java
@@ -3,20 +3,26 @@ package com.castro.estrutura.vetor;
 public class Vetor {
 
 	private String[] elementos;
+	private int tamanhoReal;
 
 	public Vetor(int capacidade) {
 
 		this.elementos = new String[capacidade];
+		this.tamanhoReal = 0;
 	}
-
-	public void adiciona(String elemento) {
-
-		for (int i = 0; i < this.elementos.length; i++) {
-			
-			if (this.elementos[i] == null) {
-				this.elementos[i] = elemento;
-				break;
-			}
+	
+	/*
+	 *  another method with boolean return in place of the exception
+	 */
+	public void adiciona(String elemento) throws Exception {
+		
+		if(this.tamanhoReal < this.elementos.length) {
+			this.elementos[tamanhoReal] = elemento;
+			this.tamanhoReal ++;
+		} else {
+			throw new Exception("Vetor já está cheio, impossível adicionar o elemento!");
 		}
+		
+		
 	}
 }
--- src/com/castro/estrutura/vetor/teste/Aula3TesteAddElemento.java
+++ src/com/castro/estrutura/vetor/teste/Aula3TesteAddElemento.java
@@ -6,10 +6,17 @@ public class Aula3TesteAddElemento {
 
 	public static void main(String[] args) {
 
-		Vetor vetor = new Vetor(5);
+		Vetor vetor = new Vetor(2);
 		
-		vetor.adiciona("item 1");
-		vetor.adiciona("item 2");
+		try {
+			vetor.adiciona("item 1");
+			vetor.adiciona("item 2");
+			vetor.adiciona("item 3");
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
 	}
 
 }
